5945c88e64ce3a140a86a78aa2ff6effcdda761a
pedroangelalcantaraalcaide <pedroangelalcantaraalcaide@pc212-8.14003551.14.andared.ced.junta-andalucia.es>
2018-04-11 09:35:30 +0200

    Realizado v- Extraer superclase.

---
+++ src/practica/pkg7/pkg2/superclase.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg7.pkg2;
+
+/**
+ *
+ * @author pedroangelalcantaraalcaide
+ */
+public class superclase {
+    
+    protected String dni;
+    protected String nombre;
+
+    public superclase() {
+    }
+
+    /**
+     *Nos devuelve el DNI
+     * @return Devuelve el dato dni como un String
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * Su función es introducir el valor dni
+     * @param dni es el dato dni como tipo de dato String
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     *Nos devuelve el Nombre
+     * @return Devuelve el nombre dni como un String
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     *Su función es introducir el valor Nombre
+     * @param nombre es el dato nombre como tipo de dato String
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
